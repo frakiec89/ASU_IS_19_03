e9cccd7a27682ca52b42f320839b53092e51647c
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2021-10-01 10:13:28 +0400

    Добавил контекстное  меню  для удаления склада

--- ASU_IS_19_03/Forms/SkladWindows.xaml.cs
+++ ASU_IS_19_03/Forms/SkladWindows.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASU_IS_19_03.DB.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -55,6 +56,41 @@ namespace ASU_IS_19_03.Forms
                 if (
                     MessageBox.Show(message , 
                     "Удаление" , MessageBoxButton.YesNo )
+                    == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        DB.MsSqlContext context = new DB.MsSqlContext();
+                        context.Sklads.Remove(sklad);
+                        context.SaveChanges();
+                        MessageBox.Show("Удаление  прошло");
+
+                        DB.MsSqlContext sqlContext = new DB.MsSqlContext(); // экземпляр подключения
+                        var content = sqlContext.Sklads.ToList(); // получили коллекцию 
+                        datagridSklad.ItemsSource = content;// отдали коллекцию таблице
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var menuItem = (MenuItem)sender;
+            var contextMenu = (ContextMenu)menuItem.Parent;
+            var item = (DataGrid)contextMenu.PlacementTarget;
+            var sklad = item.SelectedItem as Sklad;
+          
+            if (sklad != null)
+            {
+                string message = $"Вы уверены что хотите удалить: \n {sklad.Adress} из базы данный";
+
+                if (
+                    MessageBox.Show(message,
+                    "Удаление", MessageBoxButton.YesNo)
                     == MessageBoxResult.Yes)
                 {
                     try
